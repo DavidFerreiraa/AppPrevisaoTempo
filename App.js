d19ffc3fd19e1a4da3b14b6a62583d441dc73e89
David <dfl8@aluno.ifal.edu.br>
2022-09-26 07:16:58 -0300

    Início da adição de navegações entre telas

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
 import InitPage from "./src/views/InitPage";
 import Page02 from './src/views/Page02';
 
 export default function App() {
   return (
     <View>
-      <Page02/>
+      <NavigationContainer>
+        <Page02/>
+      </NavigationContainer>
       <StatusBar style="auto"/>
     </View>
   );
